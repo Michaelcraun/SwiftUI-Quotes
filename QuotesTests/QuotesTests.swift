6aa66257f57ab2296e7e539043f6ef30798521f5
Michael <michael.craun@gmail.com>
2020-04-28 17:38:51 -0400

    Initial Commit

---
+++ QuotesTests/QuotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuotesTests.swift
+//  QuotesTests
+//
+//  Created by Michael Craun on 4/28/20.
+//  Copyright © 2020 Craunic Productions. All rights reserved.
+//
+
+import XCTest
+@testable import Quotes
+
+class QuotesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
